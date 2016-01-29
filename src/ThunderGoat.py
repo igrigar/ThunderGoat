df493db7a6f0add6b81ef1f76dee9190cfdc6098
Diego Vicente <diegovicente@protonmail.com>
2016-01-29 11:22:44 +0100

    Quotes soften & Logging added

--- src/ThunderGoat.py
+++ src/ThunderGoat.py
@@ -1,22 +1,22 @@
 import os
 import sys
+import logging
 from telegram import Updater
 
 def main():
 
     bot_dir = os.path.dirname(os.path.realpath(__file__))[:-3]
     plugin_dir = bot_dir+'plugins/'
-	
-    # Get bot auth token. 
+
+    # Get bot auth token.
     token = ''
     with  open(bot_dir+'etc/token', 'r') as token_f:
         token = token_f.readline().rstrip()
-    token_f.close()
 
     # Creating the updater and the dispatcher.
     updater = Updater(token)
     dp = updater.dispatcher
-	
+
     # Import all modules.
     sys.path.append(bot_dir)
     import plugins
@@ -33,7 +33,7 @@ def main():
                     mod = 'plugins.'+module[:-3]
 
 
-                    # Create the handler.				
+                    # Create the handler.
                     var = sys.modules[mod]
                     handler = getattr(var, command)
                     continue
@@ -44,19 +44,27 @@ def main():
                     if var == 'command':
                         dp.addTelegramCommandHandler(command, handler)
                         # For debug purposes print the loaded functions and the module they belong.
-                        print 'From module:', mod, 'Function:', command, var 
+                        print 'From module:', mod, 'Function:', command, var
                     if var == 'message':
-			dp.addTelegramMessageHandler(handler)
+            dp.addTelegramMessageHandler(handler)
                         # For debug purposes print the loaded functions and the module they belong.
-                        print 'From module:', mod, 'Function:', command, var 
+                        print 'From module:', mod, 'Function:', command, var
                     if var == 'daemon':
                         handler()
                         print 'From module:', mod, 'Function:', command, var
 
-    del module	
-	
+    del module
+
+    # Enable logging
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
+
+
     updater.start_polling()
-    updater.idle()	
+    updater.idle()
 
 if __name__ == '__main__':
     main()
